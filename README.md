dc37336916b00c5a37e39c30e512e2d195a6886c
BeNikk <bhattnik442@gmail.com>
2025-08-09 01:47:55 +0530

    update:readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pair-program
 Program with your friends
+
+## Join rooms
+![Home](./frontend/public/image.png)
+
+## Code Together
+![Room](./frontend/public/image-1.png)
--- backend/src/ws/socket.ts
+++ backend/src/ws/socket.ts
@@ -24,7 +24,12 @@ export function setupWebSocket(server: http.Server) {
         currentRoomId = roomId;
         currentUserName = userName;
         if (!rooms[roomId]) {
-          rooms[roomId] = { clients: new Set(), code: "", users: new Map() };
+          rooms[roomId] = {
+            clients: new Set(),
+            code: "",
+            question: null,
+            users: new Map(),
+          };
         }
         rooms[roomId].clients.add(ws);
         rooms[roomId].users.set(ws, userName);
@@ -49,6 +54,14 @@ export function setupWebSocket(server: http.Server) {
           rooms,
           ws
         );
+        if (rooms[roomId].question) {
+          ws.send(
+            JSON.stringify({
+              type: "QUESTION_UPDATE",
+              question: rooms[roomId].question,
+            })
+          );
+        }
       } else if (parsedData.type == "CODE_CHANGE") {
         const { codeChange } = parsedData;
         if (currentRoomId && rooms[currentRoomId]) {
@@ -67,6 +80,21 @@ export function setupWebSocket(server: http.Server) {
       } else if (parsedData.type === "LEAVE_ROOM" && currentRoomId) {
         leaveRoom(ws, currentRoomId, currentUserName);
         currentRoomId = null;
+      } else if (parsedData.type === "SET_QUESTION") {
+        const { question } = parsedData;
+        if (currentRoomId && rooms[currentRoomId]) {
+          rooms[currentRoomId]!.question = question;
+
+          broadcast(
+            currentRoomId,
+            {
+              type: "QUESTION_UPDATE",
+              question,
+              userName: currentUserName,
+            },
+            rooms
+          );
+        }
       }
     });
     ws.on("close", () => {
